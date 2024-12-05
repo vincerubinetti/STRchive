78917994aa7cdf80ba5d193f5942ac9ac51e1ff8
Vincent Rubinetti <vince.rubinetti@gmail.com>
2024-12-04 20:56:20 -0500

    version replace

--- .github/workflows/version.js
+++ .github/workflows/version.js
@@ -3,21 +3,20 @@ const { lte, valid } = require("semver");
 const { parse } = require("yaml");
 const { globSync } = require("glob");
 
-module.exports = (glob) => {
-  // load and parse file contents
-  const { version: newVersion } = parse(
-    readFileSync("CITATION.cff").toString()
-  );
-  const { version: oldVersion } = parse(
-    readFileSync("CITATION-previous.cff").toString()
-  );
+// load and parse file contents
+const { version: newVersion } = parse(readFileSync("CITATION.cff").toString());
+const { version: oldVersion } = parse(
+  readFileSync("CITATION-previous.cff").toString()
+);
 
-  // check version
-  if (!valid(newVersion)) throw Error("Version not valid");
-  if (lte(newVersion, oldVersion)) return;
+// check version
+if (!valid(newVersion)) throw Error("Version not valid");
+if (lte(newVersion, oldVersion)) return;
 
-  // add version to artifact filenames
-  for (const artifact of globSync(glob)) renameSync(artifact, artifact);
+// add version to artifact filenames
+for (const artifact of globSync("data/STRchive-*.json"))
+  renameSync(artifact, artifact.replace(".json", `_v${newVersion}.json`));
+for (const artifact of globSync("data/STRchive-disease-loci*.bed"))
+  renameSync(artifact, artifact.replace("-loci", `-loci_v${newVersion}_`));
 
-  return version;
-};
+return version;
